295be499593331834767edc1d8faf4a84574b91f
Ernest Dragomir <ejustinidb90@educastur.es>
2025-10-07 18:35:09 +0200

    Ejercicio 2 y más cosas que hice un add . en piloto automatico

---
+++ 01-javascript/js/02-solucion.js
@@ -0,0 +1,11 @@
+let nombres = ["Pepe", "Jesús", "Mariano", "Joerik", "Anita"];
+nombres.push("Push");
+nombres.shift;
+nombres.includes("Pepe")
+
+let numerosOrdenar= [4, 1, 9, 3, 7] ;
+numerosOrdenar.sort((a,b) => a-b); //Ascendente
+numerosOrdenar.sort((a,b) => b-a); //Descendente
+
+let arrayExtraer= ["manzana", "pera", "plátano", "naranja"];
+arrayExtraer.slice
